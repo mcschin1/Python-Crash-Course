4f9504808e08bd6ecf75fb16fd13e1993b03891f
CS Chin <mcschin1@users.noreply.github.com>
2025-08-20 17:12:58 +0800

    Add files via upload

---
+++ Vehicle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start_engine(self):
+        print("Engine started.")
+
